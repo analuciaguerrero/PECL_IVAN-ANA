8ab6f1b07b02e314a202598eb4314e3c1d064d26
analuciaguerrero <157589298+analuciaguerrero@users.noreply.github.com>
2025-05-09 20:12:07 +0200

    1

---
+++ src/main/java/es/uah/matcomp/mp/ed/proyectofinal/pecl_ivanana/HelloController.java
@@ -0,0 +1,14 @@
+package es.uah.matcomp.mp.ed.proyectofinal.pecl_ivanana;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.uah.matcomp.mp.ed.proyectofinal.pecl_ivanana {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.uah.matcomp.mp.ed.proyectofinal.pecl_ivanana to javafx.fxml;
+    exports es.uah.matcomp.mp.ed.proyectofinal.pecl_ivanana;
+}
